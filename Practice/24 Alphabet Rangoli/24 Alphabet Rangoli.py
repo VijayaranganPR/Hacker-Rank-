658255129faf08ae0caa8899db30fba8e256da00
Vijayarangan Pandurengadurai Raju <prvijayarangans@gmail.com>
2023-04-17 14:23:14 +0530

    Changing 24 Rangoli

--- Practice/24 Alphabet Rangoli/24 Alphabet Rangoli.py
+++ Practice/24 Alphabet Rangoli/24 Alphabet Rangoli.py
@@ -1,22 +1,12 @@
-def print_rangoli(n):
-    
-    ifen_size = n*2-2
-    a = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-    alp = a[:n]
-    alp = alp[::-1]
-    st = '-'.join(alp)+'0'
-
-    l = []
-    for i in range(ifen_size,0,-2):
-        temp = '-'*i+st[:ifen_size-i]
-        l.append(temp+alp[n-1-int(i/2)]+temp[::-1])
-        
-    st = st[:-1] +st[-3::-1]
-
-    l +=[st]+l[::-1]
-    for i in l:
-        print(i)
-
+def print_rangoli(size):
+    if (size == 1):
+        print('a')
+        return
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    alphabet = alphabet[size-1::-1]
+    space = size*4 - 3
+    pattern = ['-'.join(alphabet[:i+1] + alphabet[::-1][size-i:]).center(space, '-') for i in range(size)]
+    print('\n'.join(pattern + pattern[size-2::-1]))
 if __name__ == '__main__':
     n = int(input())
     print_rangoli(n)
