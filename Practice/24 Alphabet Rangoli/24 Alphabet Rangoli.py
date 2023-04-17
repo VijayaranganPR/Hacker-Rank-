aa769c05002a90280d7d58f22f0addfa05b5713f
Vijayarangan Pandurengadurai Raju <prvijayarangans@gmail.com>
2023-04-17 17:06:29 +0530

    more optimized 24 Rangoli

--- Practice/24 Alphabet Rangoli/24 Alphabet Rangoli.py
+++ Practice/24 Alphabet Rangoli/24 Alphabet Rangoli.py
@@ -1,12 +1,8 @@
 def print_rangoli(size):
-    if (size == 1):
-        print('a')
-        return
-    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    alphabet = alphabet[size-1::-1]
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'][size-1::-1]
     space = size*4 - 3
     pattern = ['-'.join(alphabet[:i+1] + alphabet[::-1][size-i:]).center(space, '-') for i in range(size)]
-    print('\n'.join(pattern + pattern[size-2::-1]))
+    print ('\n'.join(pattern + pattern[-2::-1]))
 if __name__ == '__main__':
     n = int(input())
     print_rangoli(n)
