53f01bbaf461eb28614204dc6444fc1d1e6f861b
Vijayarangan Pandurengadurai Raju <prvijayarangans@gmail.com>
2023-04-17 17:22:34 +0530

    changing 25 Rangoli

--- Practice/25 Capitalize/25 Capitalize.py
+++ Practice/25 Capitalize/25 Capitalize.py
@@ -8,7 +8,7 @@ import sys
 
 # Complete the solve function below.
 def solve(s):
-    for x in s[:].split():
+    for x in s.split():
         s = s.replace(x, x.capitalize())
     return(s)
 if __name__ == '__main__':
