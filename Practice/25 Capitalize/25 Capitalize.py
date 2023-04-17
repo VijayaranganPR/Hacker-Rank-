bf187b30ebc2cdcbc54772b9fa87f89fa22d23eb
Vijayarangan Pandurengadurai Raju <prvijayarangans@gmail.com>
2023-04-17 17:23:52 +0530

    changing 25

--- Practice/25 Capitalize/25 Capitalize.py
+++ Practice/25 Capitalize/25 Capitalize.py
@@ -6,7 +6,7 @@ import random
 import re
 import sys
 
-# Complete the solve function below.
+# Complete the solve function below
 def solve(s):
     for x in s.split():
         s = s.replace(x, x.capitalize())
