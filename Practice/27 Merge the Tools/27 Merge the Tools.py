4cd12bc6b25b904abb3eeb0ebee7e21d04bf26f2
Vijayarangan Pandurengadurai Raju <prvijayarangans@gmail.com>
2023-04-19 22:01:32 +0530

    Changing 27 Merge the tools

--- Practice/27 Merge the Tools/27 Merge the Tools.py
+++ Practice/27 Merge the Tools/27 Merge the Tools.py
@@ -1,16 +1,13 @@
 def merge_the_tools(string, k):
-    l = [string[i:i+k] for i in range(0,len(string),k)]
-    result = []
-    for i in l:
-        temp = ""
-        for j in i:
-            if j in temp:
-                continue
-            else:
-                temp+=j
-        result.append(temp)
-    print(*result,sep = "\n")
-
+    final = []
+    for i in range(k, len(string) + k, k):
+        min_string = ''
+        for j in string[i-k: i]:
+            if(j not in min_string):
+                min_string += j
+        final.append(min_string)
+    print('\n'.join(final))
+        
 if __name__ == '__main__':
     string, k = input(), int(input())
     merge_the_tools(string, k)
