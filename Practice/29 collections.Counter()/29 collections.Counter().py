72749b7d911994cf53ba15a1195a2828eeb3af8e
Vijayarangan Pandurengadurai Raju <prvijayarangans@gmail.com>
2023-04-20 11:30:28 +0530

    Changing 29 collections.Counter()

--- Practice/29 collections.Counter()/29 collections.Counter().py
+++ Practice/29 collections.Counter()/29 collections.Counter().py
@@ -1,14 +1,11 @@
-from collections import Counter
-total_size = int(input())
-size_dict = Counter(map(int,input().split()))
-number_of_costomers = int(input())
+import collections
+x = int(input())
+all_shoes = collections.Counter(map(int, input().split()))
+total_earning = 0
 
-
-money_earned = 0
-
-for _ in range(number_of_costomers):
-    shoe_size, price = map(int,input().split())
-    if size_dict[shoe_size]:
-        money_earned += price
-        size_dict[shoe_size] -=1
-print(money_earned)    
+for i in range(int(input())):
+    size, price = map(int, input().split())
+    if all_shoes[size]:
+        all_shoes[size] -= 1
+        total_earning += price
+print(total_earning)
