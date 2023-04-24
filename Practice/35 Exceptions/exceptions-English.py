cf1e7a1c26acd14c3b6fff117e59ac731f9ab6ca
Vijayarangan Pandurengadurai Raju <prvijayarangans@gmail.com>
2023-04-24 19:30:45 +0530

    Completed 35 Exceptions

--- Practice/35 Exceptions/exceptions-English.py
+++ Practice/35 Exceptions/exceptions-English.py
@@ -1,6 +1,6 @@
 for i in range(int(input())):
     try:
         a,b = map(int, input().split())
-        print(a/b)
+        print(a//b)
     except Exception as e:
         print(f'Error Code: {e}')
