a6f75434f4f46bac514242f8118eed809da632cd
mrvijaysgithub <prvijay1706@gmail.com>
2023-10-24 13:40:16 -0400

    adding 66 Triangle Quest

---
+++ Practice/66 Triangle Quest/Triangle Quest.py
@@ -0,0 +1,2 @@
+for i in range(1,int(input())): #More than 2 lines will result in 0 score. Do not leave a blank line also. Only need to use arithmatic operators and no string convertion.
+    print(i*(pow(10, i) - 1)//9)
