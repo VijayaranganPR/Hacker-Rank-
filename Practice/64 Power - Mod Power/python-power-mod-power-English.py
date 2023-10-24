ae751330622040571d4d3ec4d0d8f1734d03c1f6
mrvijaysgithub <prvijay1706@gmail.com>
2023-10-24 13:04:30 -0400

    adding 64 Power - Mod Power

---
+++ Practice/64 Power - Mod Power/python-power-mod-power-English.py
@@ -0,0 +1,10 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+# pow(a,b) -> a**b 
+# pow(a,b,c) -> (a**b)%c
+
+# printing the output in right format
+print(pow(a, b))
+print(pow(a, b, c))
